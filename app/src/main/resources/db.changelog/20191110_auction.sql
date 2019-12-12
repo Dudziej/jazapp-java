6364005377e7d7ff082e5ea1579eaee24bb5be04
Dudziej <dudzik32@gmail.com>
2019-12-12 23:25:50 +0100

    miniatura

--- app/src/main/resources/db.changelog/20191110_auction.sql
+++ app/src/main/resources/db.changelog/20191110_auction.sql
@@ -8,7 +8,7 @@ CREATE TABLE auction
 (
     id          serial       PRIMARY KEY,
     name        VARCHAR      NOT NULL,
-    price       DECIMAL(2)   NOT NULL,
+    price       DECIMAL(14,2)   NOT NULL,
     description VARCHAR      NOT NULL,
     photo       VARCHAR      NOT NULL
 );
